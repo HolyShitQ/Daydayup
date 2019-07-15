41aabd1dbb2f2b5df12df3734d654b6a5e1f6949
Ray <1070628782@qq.com>
2019-07-15 16:28:00 +0800

    heoolworld

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public Class HelloWorld{
+	public static void main(String args[]){
+		System.out.println("HelloWorld");
+		System.out.println("111111");
+		System.out.println("HelloWorld");
+		System.out.println("222222");
+
+	}
+}
