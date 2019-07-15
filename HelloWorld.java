d681fa55e86b4fbe9aee84c715612b89089aa762
小七 <48201476+HolyShitQ@users.noreply.github.com>
2019-07-15 21:35:47 +0800

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public Class HelloWorld{
-	public static void main(String args[]){
-		System.out.println("HelloWorld");
-		System.out.println("111111");
-		System.out.println("HelloWorld");
-		System.out.println("222222");
-
-	}
-}
