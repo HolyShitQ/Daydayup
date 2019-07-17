e664fe81860c31bf59e4fd5755c3b4eaa2ef4f76
Ray <1070628782@qq.com>
2019-07-17 22:03:38 +0800

    获取user信息

--- src/main/java/com/ddup/controller/AuthorizeController.java
+++ src/main/java/com/ddup/controller/AuthorizeController.java
@@ -1,14 +1,31 @@
 package com.ddup.controller;
 
+import com.ddup.dto.AccessTokenDTO;
+import com.ddup.dto.GithubUser;
+import com.ddup.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_uri("http://localhost:8888/callback");
+        accessTokenDTO.setClient_id("3eabf8aa4a4e848e7244");
+        accessTokenDTO.setClient_secret("1bd529753336b35f9af99f5771e49273cd98ae98");
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
         return "index";
     }
 }
