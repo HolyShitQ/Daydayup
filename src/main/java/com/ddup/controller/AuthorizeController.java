b2346c131055ed91648daf6ca02f5a98907a1cd3
Ray <1070628782@qq.com>
2019-07-17 22:21:21 +0800

    数据分离到配置文件中

--- src/main/java/com/ddup/controller/AuthorizeController.java
+++ src/main/java/com/ddup/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.ddup.dto.AccessTokenDTO;
 import com.ddup.dto.GithubUser;
 import com.ddup.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +15,22 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setRedirect_uri("http://localhost:8888/callback");
-        accessTokenDTO.setClient_id("3eabf8aa4a4e848e7244");
-        accessTokenDTO.setClient_secret("1bd529753336b35f9af99f5771e49273cd98ae98");
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user.getName());
