31c8efc94c5fd6bb9488a0fd7874a0f7917952cf
Ray <1070628782@qq.com>
2019-07-30 14:43:28 +0800

    发布功能实现

---
+++ src/main/java/com/ddup/mapper/QuestionMapper.java
@@ -0,0 +1,11 @@
+package com.ddup.mapper;
+
+import com.ddup.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QuestionMapper {
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
+    void create(Question question);
+}
