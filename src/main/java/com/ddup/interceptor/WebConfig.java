968594ce5b0adf25b6215872739b08544970fa6d
Ray <1070628782@qq.com>
2019-08-13 20:57:35 +0800

    去掉EnableWebMvc注解，解决css和js文件被拦截的问题

--- src/main/java/com/ddup/interceptor/WebConfig.java
+++ src/main/java/com/ddup/interceptor/WebConfig.java
@@ -2,12 +2,11 @@ package com.ddup.interceptor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Autowired
@@ -17,4 +16,9 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(sessionInterceptor).addPathPatterns("/**");
     }
+
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//
+//    }
 }
