7519d4dca44105e6cb8c83bcce63faeac88f8f88
Ray <1070628782@qq.com>
2019-08-14 15:06:06 +0800

    首页点击问题，可以查看问题详细1

---
+++ src/main/java/com/ddup/controller/QuestionController.java
@@ -0,0 +1,24 @@
+package com.ddup.controller;
+
+import com.ddup.dto.QuestionDTO;
+import com.ddup.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class QuestionController {
+
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("question/{id}")
+    public String question(@PathVariable(name = "id") Integer id,
+                           Model model){
+        QuestionDTO questionDTO = questionService.getById(id);
+        model.addAttribute("question", questionDTO);
+        return "question";
+    }
+}
