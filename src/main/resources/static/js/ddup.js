a72d61a9ddbe5a6abfae2277dd3a6ea0487b1f3f
Ray <1070628782@qq.com>
2019-08-29 20:36:00 +0800

    不登录进行评论，提示登录，自动关闭登陆成功页面

--- src/main/resources/static/js/ddup.js
+++ src/main/resources/static/js/ddup.js
@@ -10,11 +10,19 @@ function post(){
             "content":content,
             "type":1
         }),
-        success: function (Response) {
-            if(Response.code == 200){
+        success: function (res) {
+            if(res.code == 200){
                 $("#comment_section").hide();
             }else {
-                alert(Response.message);
+                if(res.code == 2003){
+                    var isAccepted = confirm(res.message);
+                    if(isAccepted){
+                        window.open("https://github.com/login/oauth/authorize?client_id=3eabf8aa4a4e848e7244&redirect_uri=http://localhost:8888/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                }else {
+                    alert(Response.message);
+                }
             }
         },
         dataType:"json"
