48f3b78dc256c3ae914efbed88555b06f3fe4631
Ray <1070628782@qq.com>
2019-09-02 22:24:25 +0800

    优化回复功能

--- src/main/resources/static/js/ddup.js
+++ src/main/resources/static/js/ddup.js
@@ -1,30 +1,36 @@
-function post(){
+function post() {
     var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
+
+    if (!content) {
+        alert("不能回复空内容~~~");
+        return;
+    }
+
     $.ajax({
         type: "post",
         url: "/comment",
         contentType: "application/json",
         data: JSON.stringify({
-            "parentId":questionId,
-            "content":content,
-            "type":1
+            "parentId": questionId,
+            "content": content,
+            "type": 1
         }),
-        success: function (res) {
-            if(res.code == 200){
-                $("#comment_section").hide();
-            }else {
-                if(res.code == 2003){
-                    var isAccepted = confirm(res.message);
-                    if(isAccepted){
+        success: function (response) {
+            if (response.code == 200) {
+                window.location.reload();
+            } else {
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
                         window.open("https://github.com/login/oauth/authorize?client_id=3eabf8aa4a4e848e7244&redirect_uri=http://localhost:8888/callback&scope=user&state=1");
                         window.localStorage.setItem("closable", true);
                     }
-                }else {
-                    alert(Response.message);
+                } else {
+                    alert(response.message);
                 }
             }
         },
-        dataType:"json"
+        dataType: "json"
     });
 }
