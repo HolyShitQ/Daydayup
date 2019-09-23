6dad5703bb7d4491b00f6657f9265a58427dfd43
Ray <1070628782@qq.com>
2019-09-23 20:26:39 +0800

    定时器schedule测试

--- src/main/java/com/ddup/DdupApplication.java
+++ src/main/java/com/ddup/DdupApplication.java
@@ -3,9 +3,11 @@ package com.ddup;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.ddup.mapper")
+@EnableScheduling
 public class DdupApplication {
 
     public static void main(String[] args) {
